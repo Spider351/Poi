16edb2c284438d4c606be656aa378bdf7fdaefb2
Javier Ornelas Valles <naitomea351@gmail.com>
2018-11-25 02:04:26 -0600

    POI

--- 01-PrimerAdelanto/src/public/js/main.js
+++ 01-PrimerAdelanto/src/public/js/main.js
@@ -5,6 +5,27 @@ $(function(){
     const msgView = $("#viewMsg");
     const msgContent = $("#contentMsg");
 
+    const formNick = $("#nickForm");
+    const errorNick = $("#nickError");
+    const nickName = $("#nick");
+
+    const usersList = $("#users");
+
+    formNick.submit(function(e){
+        e.preventDefault();
+        socket.emit("nuevo usuario", nickName.val(), function(data){
+            console.log("agh");
+            if(data){
+                $("#nickWrap").hide();
+                $("#contentWrap").show();
+            }
+            else{
+                errorNick.html("<div class='alert alert-danger'>Error: Usuario ya existente</div>");
+            }
+            nickName.val(" ");
+        });
+    });
+
     msgForm.submit(function(e){
         e.preventDefault();
         socket.emit("enviar mensaje", msgContent.val());
@@ -12,6 +33,16 @@ $(function(){
     });
 
     socket.on("recibir mensaje", function(msg){
-        msgView.append(msg + "<br>");
+        msgView.append("<b>" + msg.nick + ": </b>" + msg.msg + "<br>");
+    });
+
+    socket.on("usuarios", function(msg){
+        let html = "";
+        for (let i = 0; i < msg.length; i++){
+            html += "<p>"
+            html += msg[i]
+            html += "</p>"
+        }
+        usersList.html(html);
     });
 })
--- 01-PrimerAdelanto/src/sockets.js
+++ 01-PrimerAdelanto/src/sockets.js
@@ -1,8 +1,40 @@
 module.exports = function(io){
+
+    let nicknames = [];
+
     io.on("connection", function(socket){
         console.log("Usuario Conectado");
-        socket.on("enviar mensaje", function(msg){
-            io.sockets.emit("recibir mensaje", msg);
+
+        socket.on("nuevo usuario", function(data, cb){
+            console.log(data);
+            if(nicknames.indexOf(data) != -1){
+                cb (false);
+            }
+            else{
+                cb(true);
+                socket.nickname = data;
+                nicknames.push(data);
+                io.sockets.emit("usuarios", nicknames);
+            }
         });
+
+        socket.on("enviar mensaje", function(msg){
+            io.sockets.emit("recibir mensaje", {
+                msg : msg,
+                nick : socket.nickname
+            });
+        });
+
+        socket.on("disconnect", function(data){
+            if(!socket.nickname){
+                return;
+            }
+            else{
+                nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+                io.sockets.emit("usuarios", nicknames);
+            }
+
+        });
+
     });
 }
